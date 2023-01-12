9c670ac53dab6d491a73d0e9d7cacc411aa2d8d7
dvirharel1 <dvir44555544@gmail.com>
2023-01-12 13:56:18 +0200

    initual commmit

---
+++ project dvir work.py
@@ -0,0 +1,16 @@
+class CardGame :
+    def __init__(self, player_name1:str, player_name2:str,  cards_amount:int = 26 ):
+        if cards_amount > 26 or cards_amount < 10:
+            cards_amount=26
+        self.player_name1=player_name1
+        self.player_name2=player_name2
+        self.cards_amount=cards_amount
+
+
+    import random
+    myfile.py
+
+    class DeckOfCards
+    def New_Game
+        DeckOfCards
+
